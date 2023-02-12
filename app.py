8586f84adcffa582cca5abf8c5f593ccf1e14139
Omar <o.moreta@gmail.com>
2023-02-12 05:47:50 -0500

    created application factory and created flask instance in app.py

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
 # CONFIG
-from flask import Flask
+from animals import create_app;
 
-# CREATE APP INSTANCE
-app = Flask(__name__)
-
-# INDEX ROUTE
-
-
-@app.route("/")
-def index():
-    return "Hello, this is an Animal Hangout Spot"
-
-# ANIMAL INDEX ROUTE
-
-
-@app.route("/animals")
-def animals():
-    return "These are the pets that are just chillin' at the Animal Hangout Spot"
+# FLASK INSTANCE CREATED BY APP FACTORY
+app = create_app()
